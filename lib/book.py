d7c5558386ac4576f12edc3ee5c2eb6c62a519cc
Shawn Webster <swolfgangwebster@gmail.com>
2023-07-22 21:53:41 -0500

    property

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def set_page(self, page):
+        if type(page) != int:
+            print("page_count must be an integer")
+        self._page_count = page
+
+    def get(self):
+        return self._page_count
+
+    page_count = property(get, set_page)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, s):
+        if type(s) != int:
+            print("size must be an integer")
+        self._size = s
+
+    size = property(get_size, set_size)
+
     pass
